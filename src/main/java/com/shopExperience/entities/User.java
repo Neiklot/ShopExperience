b3bca18f9c390ba2969ac3f49edd1e099531e3cc
Daniel Benitez <dbenitez83@live.com>
2014-07-27 20:44:55 +0200

    User and Shop classes

---
+++ src/main/java/com/shopExperience/entities/User.java
@@ -0,0 +1,88 @@
+package com.shopExperience.entities;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+@Entity(name = "User")
+@NamedQueries({
+@NamedQuery(name="User.findAll",query="SELECT u FROM User u")})
+public class User {
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name="User_NAME",nullable=false, length = 50)
+    private String UserName;
+
+    @Column(name="PASSWORD",nullable=false, length = 50)
+    private String password;
+     
+    @ManyToOne 
+    @JoinColumn(name="association_id")
+    @LazyCollection(LazyCollectionOption.FALSE) 
+ 	private Association association;
+    
+    @ManyToOne
+    @JoinColumn(name="shop_id")
+    @LazyCollection(LazyCollectionOption.FALSE) 
+    private Shop shop;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return UserName;
+	}
+
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Association getAssociation() {
+		return association;
+	}
+
+	public void setAssociation(Association association) {
+		this.association = association;
+	}
+
+	public Shop getShop() {
+		return shop;
+	}
+
+	public void setShop(Shop shop) {
+		this.shop = shop;
+	}
+
+	
+}
---
+++ src/main/java/com/shopExperience/pagination/ModelTableShop.java
@@ -0,0 +1,23 @@
+package com.shopExperience.pagination;
+
+public class ModelTableShop {
+	
+	private int shopId;
+	private String shopName;
+
+	public int getShopId() {
+		return shopId;
+	}
+
+	public void setShopId(int shopId) {
+		this.shopId = shopId;
+	}
+
+	public String getShopName() {
+		return shopName;
+	}
+
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+}
