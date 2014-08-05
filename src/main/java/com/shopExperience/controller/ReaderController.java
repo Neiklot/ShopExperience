c7c422c0cdeef237ab01bc6bc6b035f699e86e9a
Neiklot <dbenitez83@live.com>
2014-08-05 15:53:13 +0200

    Error user not found

--- src/main/java/com/shopExperience/controller/ReaderController.java
+++ src/main/java/com/shopExperience/controller/ReaderController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.shopExperience.entities.Card;
 
@@ -13,11 +14,20 @@ public class ReaderController {
 
 	@Autowired
 	BasicController bc;
-	
-	@RequestMapping(value = "/reading",method = RequestMethod.POST)
+
+	@RequestMapping(value = "/reading", method = RequestMethod.POST)
+	@ResponseBody
 	public String reading(@RequestParam("scanFormat") String scanFormat) {
-		Card card=bc.searchCardByBarCode(scanFormat.substring(scanFormat.indexOf(":")+2));
-		System.out.println("Reading::>"+scanFormat+" User::>"+card.getClient().getClientName());
+		if (scanFormat != null) {
+			Card card = bc.searchCardByBarCode(scanFormat.substring(scanFormat
+					.indexOf(":") + 2));
+			if (card != null) {
+				System.out.println("Reading::>" + scanFormat + " User::>"
+						+ card.getClient().getClientName());
+			}else{
+				System.out.println("Communication OK - User Not found");
+			}
+		}
 		return "ok";
 	}
 }
